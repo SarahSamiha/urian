0458945aa3dd99d4b20e45267cbe59553d62a2f8
Sarah Samiha Zaman <sarahfaizakhan@gmail.com>
2023-10-16 23:39:31 +0600

    added error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,22 @@
+import { Link, useRouteError } from "react-router-dom";
+import Navbar from "../../layouts/sharedComponents/Navbar";
 
 
 const ErrorPage = () => {
+    const error = useRouteError();
+
     return (
-        <div>
-            <h1 className="text-5xl">Error</h1>
-        </div>
+        <>
+            <div className="h-screen flex flex-col items-center justify-center gap-10">
+                <h1 className="text-4xl text-purple">
+                    {error?.data?.length ? error.data : "No Data Found"}
+                </h1>
+
+                <Link to="/" className="btn  bg-purple hover:bg-red-400 border-none text-white">
+                    Go to Home
+                </Link>
+            </div>
+        </>
     );
 };
 
--- src/pages/ServiceDetail/ServiceDetail.jsx
+++ src/pages/ServiceDetail/ServiceDetail.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ErrorPage from "../ErrorPage/ErrorPage";
 
 
 const ServiceDetail = () => {
@@ -13,29 +14,34 @@ const ServiceDetail = () => {
         toast('Appointment booked. We will reach you soon.')
     }
 
-    return (
-        <div className="lg:max-w-6xl mx-auto">
-            <div className="hero h-[500px]" style={{ backgroundImage: `url(${service.image})` }}>
-                <div className="hero-overlay bg-opacity-70"></div>
-                <div className="grid grid-cols-2 gap-12 text-peach items-center">
-                    <div>
-                        <img className="pl-14" src={service.image} alt="" />
-                    </div>
-                    <div >
-                        <h1 className="mb-5 text-5xl font-bold">{service.name}</h1>
-                        <p className="pr-14 mb-5 text-xl">
-                            {service.description}
-                        </p>
-                        <p className="pr-14 mb-5 text-xl font-bold">
-                            {service.price}
-                        </p>
-                        <button onClick={notify} className="btn bg-darkGreen hover:bg-green-800 text-white border-none px-9" to='/contactUs'>Book Appointment</button>
+    if(service){
+        return (
+            <div className="lg:max-w-6xl mx-auto">
+                <div className="hero h-[500px]" style={{ backgroundImage: `url(${service.image})` }}>
+                    <div className="hero-overlay bg-opacity-70"></div>
+                    <div className="grid grid-cols-2 gap-12 text-peach items-center">
+                        <div>
+                            <img className="pl-14" src={service.image} alt="" />
+                        </div>
+                        <div >
+                            <h1 className="mb-5 text-5xl font-bold">{service.name}</h1>
+                            <p className="pr-14 mb-5 text-xl">
+                                {service.description}
+                            </p>
+                            <p className="pr-14 mb-5 text-xl font-bold">
+                                {service.price}
+                            </p>
+                            <button onClick={notify} className="btn bg-darkGreen hover:bg-green-800 text-white border-none px-9" to='/contactUs'>Book Appointment</button>
+                        </div>
                     </div>
                 </div>
+                <ToastContainer />
             </div>
-            <ToastContainer />
-        </div>
-    );
+        );
+    }
+    else {
+        return <ErrorPage></ErrorPage>
+    }
 };
 
 export default ServiceDetail;
